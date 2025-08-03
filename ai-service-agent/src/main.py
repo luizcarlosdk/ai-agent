5b3d4d5eec5b8411c865eaaf83000ae326bd7cdc
Luiz Carlos <luizcarlosdk@hotmail.com>
2025-08-02 23:59:42 -0300

    Add simple functionality to main.py
    Signed-off-by: Luiz Carlos <luizcarlosdk@hotmail.com>

--- ai-service-agent/src/main.py
+++ ai-service-agent/src/main.py
@@ -1,2 +1,16 @@
+from fastapi import FastAPI
+import uvicorn
+import os
+from dotenv import load_dotenv
+
+app = FastAPI()
+
+@app.get("/")
+async def root():
+    return {"message": "Hello World"}
+
 if __name__ == "__main__":
-    print("project base file")
+    load_dotenv(dotenv_path="config/.env")
+    host = os.environ["HOST"]
+    port = int(os.environ["PORT"])
+    uvicorn.run(app, host=host, port=port)
